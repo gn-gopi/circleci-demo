a1305ddc96feb98dec5593a2a8eb9eeb62fb3480
gn-gopi <gopinath.nagalla@sourcefuse.com>
2024-04-05 16:27:07 +0530

    test

--- main-test.py
+++ main-test.py
@@ -2,7 +2,8 @@
 from main import Add
 
 def TestAdd():
-        assert Add(2,3) == 5        
+        assert Add(2,3) == 5
+        assert Add(5,6)==12        
         print("Add Function works correctly")
 
 if __name__ == '__main__':
